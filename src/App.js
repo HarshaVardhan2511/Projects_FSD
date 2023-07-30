d9d150821aa339f2e0536d826104dba71179fe22
Sai Harsha Vardhan <125282123+HarshaVardhan2511@users.noreply.github.com>
2023-07-31 00:06:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import logo from './photos/logo.png';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Gallery from './Route/Gallery';
+import Cart from './Route/Cart';
+import data from './Data.js';
+import { useState } from 'react';
 
 function App() {
+  const [cartProducts, setCartProducts] = useState([]);
+  const handleAddToCart = (product) => {
+    setCartProducts((cartProducts) => [...cartProducts, { item: product, quantity: 1 }]);
+  };
+
+  const { productItems } = data;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar logo={logo} />
+        <Routes>
+          <Route path="/" element={<Gallery productItems={productItems} handleAddToCart={handleAddToCart} />} />
+          <Route path="/cart" element={<Cart cartProducts={cartProducts} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
